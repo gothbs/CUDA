4256857963b08fd54a490fee4971106645052f7f
Ishimwe Elis <elis_45@live.fr>
2023-05-18 20:52:11 +0200

    correction &ier iteration glaussien

--- flouGaussienOpt.cu
+++ flouGaussienOpt.cu
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cmath>
 #include <IL/il.h>
-#include <cuda.h>
 #include <cuda_runtime.h>
 
 void normaliserNoyauGaussien(float noyauGaussien[5][5]) {
@@ -18,7 +17,7 @@ void normaliserNoyauGaussien(float noyauGaussien[5][5]) {
     }
 }
 
-__global__ void applyGaussianBlur(const unsigned char *donneesSrc, unsigned char *donneesDst, int largeur, int hauteur, int bpp, const float *noyauGaussien) {
+__global__ void applyGaussianBlur(unsigned char* donneesSrc, unsigned char* donneesDst, int largeur, int hauteur, int bpp, float* noyauGaussien) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
 
@@ -29,73 +28,67 @@ __global__ void applyGaussianBlur(const unsigned char *donneesSrc, unsigned char
         return;
     }
 
-    extern __shared__ float sharedNoyauGaussien[];
-    if (threadIdx.x < 5 && threadIdx.y < 5) {
-        sharedNoyauGaussien[threadIdx.y * 5 + threadIdx.x] = noyauGaussien[threadIdx.y * 5 + threadIdx.x];
-    }
-    __syncthreads();
-
     for (int c = 0; c < bpp; ++c) {
         float somme = 0;
         for (int ky = -2; ky <= 2; ++ky) {
             for (int kx = -2; kx <= 2; ++kx) {
-                somme += donneesSrc[((y + ky) * largeur + (x + kx)) * bpp + c] * sharedNoyauGaussien[(ky + 2) * 5 + (kx + 2)];
+                somme += donneesSrc[((y + ky) * largeur + (x + kx)) * bpp + c] * noyauGaussien[(ky + 2) * 5 + (kx + 2)];
             }
         }
         donneesDst[(y * largeur + x) * bpp + c] = somme;
     }
 }
 
-void flouGaussienGPU(unsigned char *donnees, unsigned char *nouvellesDonnees, int largeur, int hauteur, int bpp, int iterations) {
+void flouGaussienGPU(unsigned char* donnees, unsigned char* nouvellesDonnees, int largeur, int hauteur, int bpp, int iterations) {
     float noyauGaussien[5][5] = {
-        {1.0f, 4.0f,  7.0f,  4.0f, 1.0f},
+        {1.0f, 4.0f, 7.0f, 4.0f, 1.0f},
         {4.0f, 16.0f, 26.0f, 16.0f, 4.0f},
         {7.0f, 26.0f, 41.0f, 26.0f, 7.0f},
         {4.0f, 16.0f, 26.0f, 16.0f, 4.0f},
-        {1.0f, 4.0f,  7.0f,  4.0f, 1.0f}
+        {1.0f, 4.0f, 7.0f, 4.0f, 1.0f}
     };
 
     normaliserNoyauGaussien(noyauGaussien);
 
-    float *devNoyauGaussien;
-    cudaMalloc((void **)&devNoyauGaussien, 5 * 5 * sizeof(float));
+    float* devNoyauGaussien;
+    cudaMalloc((void**)&devNoyauGaussien, 5 * 5 * sizeof(float));
     cudaMemcpy(devNoyauGaussien, noyauGaussien, 5 * 5 * sizeof(float), cudaMemcpyHostToDevice);
 
-    unsigned char *devDonneesSrc, *devDonneesDst;
-    cudaMalloc((void **)&devDonneesSrc, largeur * hauteur * bpp * sizeof(unsigned char));
-    cudaMalloc((void **)&devDonneesDst, largeur * hauteur * bpp * sizeof(unsigned char));
+    dim3 blockSize(16, 16);
+    dim3 gridSize((largeur + blockSize.x - 1) / blockSize.x, (hauteur + blockSize.y - 1) / blockSize.y);
+
+    unsigned char* devDonneesSrc, * devDonneesDst;
+    cudaMalloc((void**)&devDonneesSrc, largeur * hauteur * bpp * sizeof(unsigned char));
+    cudaMalloc((void**)&devDonneesDst, largeur * hauteur * bpp * sizeof(unsigned char));
 
     cudaMemcpy(devDonneesSrc, donnees, largeur * hauteur * bpp * sizeof(unsigned char), cudaMemcpyHostToDevice);
-
-    dim3 blockDim(16, 16);
-    dim3 gridDim((largeur + blockDim.x - 1) / blockDim.x, (hauteur + blockDim.y - 1) / blockDim.y);
-
-    size_t sharedMemSize = 5 * 5 * sizeof(float);
 
     cudaStream_t stream;
     cudaStreamCreate(&stream);
 
     for (int it = 0; it < iterations; ++it) {
         if (it < iterations - 1) {
-            applyGaussianBlur<<<gridDim, blockDim, sharedMemSize, stream>>>(devDonneesSrc, devDonneesDst, largeur, hauteur, bpp, devNoyauGaussien);
-            cudaStreamSynchronize(stream);
-            std::swap(devDonneesSrc, devDonneesDst);
+            applyGaussianBlur<<<gridSize, blockSize, 0, stream>>>(devDonneesSrc, devDonneesDst, largeur, hauteur, bpp, devNoyauGaussien);
+            unsigned char* temp = devDonneesSrc;
+            devDonneesSrc = devDonneesDst;
+            devDonneesDst = temp;
         }
         else {
-            applyGaussianBlur<<<gridDim, blockDim, sharedMemSize, stream>>>(devDonneesSrc, devDonneesDst, largeur, hauteur, bpp, devNoyauGaussien);
-            cudaStreamSynchronize(stream);
+            applyGaussianBlur<<<gridSize, blockSize, 0, stream>>>(devDonneesSrc, devDonneesDst, largeur, hauteur, bpp, devNoyauGaussien);
         }
     }
 
-    cudaMemcpy(nouvellesDonnees, devDonneesDst, largeur * hauteur * bpp * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+    cudaMemcpyAsync(nouvellesDonnees, devDonneesDst, largeur * hauteur * bpp * sizeof(unsigned char), cudaMemcpyDeviceToHost, stream);
+    cudaStreamSynchronize(stream);
 
     cudaFree(devDonneesSrc);
     cudaFree(devDonneesDst);
     cudaFree(devNoyauGaussien);
+
     cudaStreamDestroy(stream);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     unsigned int image;
 
     ilInit();
@@ -110,8 +103,8 @@ int main(int argc, char *argv[]) {
     bpp = ilGetInteger(IL_IMAGE_BYTES_PER_PIXEL);
     format = ilGetInteger(IL_IMAGE_FORMAT);
 
-    unsigned char *donnees = ilGetData();
-    unsigned char *nouvellesDonnees = new unsigned char[largeur * hauteur * bpp];
+    unsigned char* donnees = ilGetData();
+    unsigned char* nouvellesDonnees = new unsigned char[largeur * hauteur * bpp];
 
     int iterations = std::stoi(argv[2]);
 
